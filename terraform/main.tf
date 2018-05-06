a1802c8cfdff0efdebf85f5daaff95f782c50eed
vladimir-kazarin <vladimir_kazarin@yahoo.com>
2018-05-06 09:35:34 -0400

    adding production server in terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,41 @@ resource "google_compute_instance_template" "tf-server-staging" {
     access_config {}
   }
 
+  tags = ["http-server"]
+
+  service_account {
+    scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/devstorage.read_write",
+    ]
+  }
+  metadata {
+      gce-container-declaration = <<EOF
+  spec:
+    containers:
+    - image: 'gcr.io/comp698-vak1003/github-vladimir-kazarin-comp698-final:d2485c699de9ed63f767024c9afaead3614cd345'
+      name: service-container
+      stdin: false
+      tty: false
+    restartPolicy: Always
+EOF
+  }
+}
+
+
+resource "google_compute_instance_template" "tf-server-prod" {
+  name = "tf-server-prod"
+  project = "comp698-vak1003"
+  disk {
+    source_image = "cos-cloud/cos-stable"
+  }
+  machine_type = "n1-standard-1"
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
   tags = ["http-server"] 
 
   service_account {
@@ -49,6 +84,15 @@ resource "google_compute_instance_group_manager" "default2-staging" {
   zone = "us-central1-f"
   base_instance_name = "staging"
   instance_template  = "${google_compute_instance_template.tf-server-staging.self_link}"
+  target_size = 1
+}
+
+resource "google_compute_instance_group_manager" "default2-prod" {
+  name = "tf-manager-prod"
+  project = "comp698-vak1003"
+  zone = "us-central1-f"
+  base_instance_name = "prod"
+  instance_template  = "${google_compute_instance_template.tf-server-prod.self_link}"
   target_size = 1
 }
 
